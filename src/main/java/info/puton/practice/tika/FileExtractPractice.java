0c59e8871c6d55dd4c96690e92a9de5f5108164c
taoyang <hackty@163.com>
2016-09-21 17:36:48 +0800

    alter a bit for FileExtractPractice

--- src/main/java/info/puton/practice/tika/FileExtractPractice.java
+++ src/main/java/info/puton/practice/tika/FileExtractPractice.java
@@ -1,5 +1,6 @@
 package info.puton.practice.tika;
 
+import info.puton.practice.tika.model.FileFullText;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
@@ -14,35 +15,36 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by taoyang on 2016/9/18.
  */
-public class ExtractPractice {
+public class FileExtractPractice {
 
-    String filePath = "D:/programming/java/practice/tika-practice/src/main/resources/一碗阳春面.docx";
-
-    public String extractToMap() {
+    public FileFullText extract(File file) {
+        FileFullText fileFullText = new FileFullText();
+        String fileKey = UUID.randomUUID().toString();
+        fileFullText.setFileKey(fileKey);//fileKey
+        fileFullText.setFileName(file.getName());//fileName
         ContentHandler handler = new BodyContentHandler();
         AutoDetectParser parser = new AutoDetectParser();
         Metadata metadata = new Metadata();
         try {
-            File file =  new File(filePath);
             InputStream stream = TikaInputStream.get(file.toPath());
             parser.parse(stream, handler, metadata);
-
-            for (String name : metadata.names()) {
+//            for (String name : metadata.names()) {
 //                System.out.println(name + ":" + metadata.get(name));
-            }
-
+//            }
+            String author = metadata.get("Author");//author
+            fileFullText.setAuthor(author);
 //            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             Date date = metadata.getDate(Property.get("Last-Modified"));
             String lastModified = sdf.format(date);
-            System.out.println(lastModified);
-
-//            System.out.println(handler.toString());
-
+            fileFullText.setModifyDate(lastModified);//modifyDate
+            String content = handler.toString();
+            fileFullText.setContent(content);//content
         } catch (SAXException e) {
             e.printStackTrace();
         } catch (TikaException e) {
@@ -50,14 +52,19 @@ public class ExtractPractice {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return handler.toString();
+        return fileFullText;
     }
 
     public static void main(String[] args) {
 
-        ExtractPractice extractPractice = new ExtractPractice();
+        String filePath = "D:/programming/java/practice/tika-practice/src/main/resources/一碗阳春面.docx";
 
-        extractPractice.extractToMap();
+        FileExtractPractice extractPractice = new FileExtractPractice();
+
+        FileFullText fileFullText = extractPractice.extract(new File(filePath));
+
+        System.out.println(fileFullText);
+
     }
 
 }
